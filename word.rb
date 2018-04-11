f22da4d829a1a8d2152b0cb7baea927e5e9a714c
scott88lee <scott88lee@gmail.com>
2018-04-11 10:38:57 +0800

    Bonus 2 smelly code

--- word.rb
+++ word.rb
@@ -1,12 +1,23 @@
 #Reworked it so now you can churn in a sentence and it outputs a sentence.
 
+#Did bonus 1 and its starting to look like a nested nightmare.
+
+#The SMELLIEST CODE YOU'll ever see for bonus 2
+
 $position = 0
 
 for value in ARGV
 	
 	test = value.downcase
+	if test[0,2] == "qu"
+		test = test[2, (test.length-1)] + "quay "
+		print test
 
-	if test[0] =~ /[aeiou]/
+	elsif test[0,3] == "squ"
+		test = test[3, (test.length-1)] + "squay "
+		print test
+
+	elsif test[0] =~ /[aeiou]/
 		print test + "way "
 	else
 		if test[0] == "y"

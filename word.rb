51aad73cc14a97a6d3f62949b1fbcc96b3fadb8d
scott88lee <scott88lee@gmail.com>
2018-04-11 10:23:16 +0800

    Bonus one

--- word.rb
+++ word.rb
@@ -9,17 +9,29 @@ for value in ARGV
 	if test[0] =~ /[aeiou]/
 		print test + "way "
 	else
-		
-		test.chars.to_a.each do |i|
-			# i == "a" || test[0] == "e" || test[0] == "i" || test[0] == "o" || test[0] == "u"
-			if i =~ /[aeiou]/
-				break
-			else
-				$position += 1
+		if test[0] == "y"
+			test.chars.to_a.each do |i|
+				# i == "a" || test[0] == "e" || test[0] == "i" || test[0] == "o" || test[0] == "u"
+				if i =~ /[aeiou]/
+					break
+				else
+					$position += 1
+				end
 			end
+			print test[$position, (test.length-1)] + test[0, $position] +"ay "
+			$position = 0
+		else
+			test.chars.to_a.each do |i|
+				# i == "a" || test[0] == "e" || test[0] == "i" || test[0] == "o" || test[0] == "u"
+				if i == "y"
+					break
+				else
+					$position += 1
+				end
+			end
+			print test[$position, (test.length-1)] + test[0, $position] +"ay "
+			$position = 0
 		end
-		print test[$position, (test.length-1)] + test[0, $position] +"ay "
-		$position = 0
 	end
 end
 puts " "
